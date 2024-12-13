dec454f84e91a15ac5d340c14a35cc8d1ef14314
snowydistant <snowydistant@gmail.com>
2024-12-13 13:57:09 -0500

    moved player properties and methods into player class

--- core/src/main/java/fiend/slayer/screens/GameScreen.java
+++ core/src/main/java/fiend/slayer/screens/GameScreen.java
@@ -1,12 +1,8 @@
 package fiend.slayer.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -14,6 +10,7 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import fiend.slayer.FiendSlayer;
+import fiend.slayer.entity.Player;
 
 public class GameScreen implements Screen {
 
@@ -23,7 +20,7 @@ public class GameScreen implements Screen {
     TiledMap tiledmap;
     OrthogonalTiledMapRenderer tiledmap_renderer = new OrthogonalTiledMapRenderer(tiledmap, 1/16f);
     ExtendViewport viewport;
-    Sprite player;
+    Player player;
 
     public GameScreen(final FiendSlayer g) {
         game = g;
@@ -35,8 +32,7 @@ public class GameScreen implements Screen {
         tiledmap = new TmxMapLoader().load("bluelevel.tmx");
         float tile_size = tiledmap.getProperties().get("tilewidth", Integer.class);
 
-        player = new Sprite(new Texture("player.png"));
-        player.setSize(1,1);
+        player = new Player(game);
 
         tiledmap_renderer = new OrthogonalTiledMapRenderer(tiledmap, 1/tile_size);
         viewport = new ExtendViewport(16, 16);
@@ -46,36 +42,23 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
         ScreenUtils.clear(Color.BLACK);
-
-        movePlayer(delta);
-
+        player.update(delta);
         viewport.apply();
-        viewport.getCamera().position.set(player.getX() + player.getWidth() / 2f, player.getY() + player.getHeight() / 2f, 0);
-
+        viewport.getCamera().position.set(player.x, player.y, 0);
         tiledmap_renderer.setView((OrthographicCamera) viewport.getCamera());
         tiledmap_renderer.render();
 
         batch.setProjectionMatrix(viewport.getCamera().combined);
         batch.begin();
+        //
 
-        player.draw(batch);
+        player.render(batch);
 
+        //
         batch.end();
 
     }
 
-    public void movePlayer(float delta) {
-        float speed = 4f;
-        if (Gdx.input.isKeyPressed(Input.Keys.W))
-            player.setPosition(player.getX(), player.getY() + speed * delta);
-        if (Gdx.input.isKeyPressed(Input.Keys.S))
-            player.setPosition(player.getX(), player.getY() - speed * delta);
-
-        if (Gdx.input.isKeyPressed(Input.Keys.A))
-            player.setPosition(player.getX() - speed * delta, player.getY());
-        if (Gdx.input.isKeyPressed(Input.Keys.D))
-            player.setPosition(player.getX() + speed * delta, player.getY());
-    }
 
     @Override
     public void resize(int width, int height) {
