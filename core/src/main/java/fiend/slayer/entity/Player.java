eca314923dbb30a21f4e11bd60dc64c410080915
ScripterRickster <101468308+ScripterRickster@users.noreply.github.com>
2024-12-13 14:28:36 -0500

    zxczxcz

--- core/src/main/java/fiend/slayer/entity/Player.java
+++ core/src/main/java/fiend/slayer/entity/Player.java
@@ -5,23 +5,36 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
 import fiend.slayer.FiendSlayer;
+import fiend.slayer.screens.GameScreen;
 
 public class Player {
     final FiendSlayer game;
+    final GameScreen gs;
 
     final private Sprite sprite;
     public float x, y;
 
-    public Player(final FiendSlayer g) {
+    public Player(final FiendSlayer g,final GameScreen gs) {
         sprite = new Sprite(new Texture("player.png"));
         sprite.setSize(1, 1);
 
         game = g;
+        this.gs = gs;
         x = 0; y = 0;
     }
 
+    public Rectangle getRectangle(){
+        return new Rectangle(x,y,sprite.getWidth(),sprite.getHeight());
+    }
+
     public void update(float delta) {
+
+        float prevX = x;
+        float prevY = y;
+
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             x += delta * 4f;
         }
@@ -35,7 +48,15 @@ public class Player {
             y -= delta * 4f;
         }
 
-        sprite.setPosition(x, y);
+        if(!gs.checkForCollisions()){
+            sprite.setPosition(x, y);
+        }else{
+            System.out.println("Collided with objects");
+            x = prevX;
+            y = prevY;
+        }
+
+
     }
 
     public void render(SpriteBatch batch) {
--- core/src/main/java/fiend/slayer/screens/GameScreen.java
+++ core/src/main/java/fiend/slayer/screens/GameScreen.java
@@ -37,12 +37,45 @@ public class GameScreen implements Screen {
         tiledmap = new TmxMapLoader().load("bluelevel.tmx");
         float tile_size = tiledmap.getProperties().get("tilewidth", Integer.class);
 
-        player = new Player(game);
+        player = new Player(game,this);
 
         tiledmap_renderer = new OrthogonalTiledMapRenderer(tiledmap, 1/tile_size);
         viewport = new ExtendViewport(16, 16);
 
+
+
     }
+
+    public boolean checkForCollisions() {
+        MapObjects objects = tiledmap.getLayers().get(0).getObjects();
+        for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
+            Rectangle rectangle = rectangleObject.getRectangle();
+            Rectangle plr_rect = player.getRectangle();
+            if (Intersector.overlaps(rectangle, plr_rect)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /*public boolean checkForCollisions(){
+        int objectLayerId = 0;
+        TiledMapTileLayer collisionObjectLayer = (TiledMapTileLayer)tiledmap.getLayers().get(objectLayerId);
+        MapObjects objects = collisionObjectLayer.getObjects();
+
+        for (RectangleMapObject rectangleObject : objects)){ //objects.getByType(RectangleMapObject.class)) {
+
+            Rectangle rectangle = rectangleObject.getRectangle();
+            Rectangle plr_rect = player.getRectangle();
+            System.out.println("RECT X: " + rectangle.getX() + " | RECT Y: " + rectangle.getY());
+            System.out.println("PLR RECT X: " + plr_rect.getX() + " | PLR RECT Y: " + plr_rect.getY());
+            if (Intersector.overlaps(rectangle, plr_rect)) {
+                return true;
+            }
+        }
+        return false;
+    }*/
 
     @Override
     public void render(float delta) {
